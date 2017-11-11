81e979594b93904ca000d3b472d6e34a16aec4e7
marjolaineboulingre <32574159+marjolaineboulingre@users.noreply.github.com>
2017-11-11 22:29:20 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -11,15 +11,7 @@ using namespace std;
 int main(int argc, char* argv[]) 
 {
 	Network network;
-	unsigned int time_sim(0);
-	
-	//network.run2neurons(time_sim);
-	//network.run(time_sim);
-	
-	//network.saveTimeSpikes();
-
 	network.simulation(C::time_simulation);
-	
 	
 	return 0;
 }
